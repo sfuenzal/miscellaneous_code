b789e042070a6a521c8b8332743fa3a2596350ce
Sebastian Fuenzalida Garrido <sebastian.fuenzalida.garrido@cern.ch>
2021-04-08 17:55:38 -0400

    Gauss Seidel algorithm added

---
+++ GaussSeidelMethod.C
@@ -0,0 +1,26 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TGraphErrors.h"
+#include "TF1.h"
+#include "TH1F.h"   
+#include "TLegend.h"
+#include "TFile.h"  
+#include "TArrow.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TMultiGraph.h"
+
+#include <iostream>
+#include <math.h>
+#include <vector> 
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+
+
+void GaussSeidelMethod()
+{
+
+}
